5965ac44605738fee8e7f2550028d5737d954497
vivek.brohma <vivek.brohma@gmail.com>
2019-07-16 22:19:55 +0200

    Trying something

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,15 @@
 
 set -x
 
-kill $(cat ../server/pid) || true
+kill $(cat ../server/pid) || true &&
 
-cd $(dirname ${BASH_SOURCE[0]})
+cd $(dirname ${BASH_SOURCE[0]}) &&
 
-cd ../web
-npm ci;
-npm run build;
+cd ../web &&
+npm ci &&
+npm run build &&
 
-cd ../server
-npm ci;
+cd ../server &&
+npm ci &&
 
 bash ../scripts/run.sh
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../server
+cd ../server &&
 
 npm start &
 
